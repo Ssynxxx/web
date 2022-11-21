e29f6cdb2a9e7990af805c2694c1ff94820b0a35
Webee21 <william.535210071@stu.untar.ac.id>
2022-11-21 16:56:30 +0700

    update local 8080

--- server.js
+++ server.js
@@ -1,23 +1,14 @@
 var express = require('express');
 const ejs = require('ejs');
 
-const config = require('./public/modules/config');
-const port = config.port;
-
 var app = express();
 
 app.use(express.static('public'));
 
 app.set('view engine', 'ejs');
 
-app.use(express.urlencoded({ extended: true }));
+app.listen(8080);
 
-app.use(express.json());
-
-const db = require('./public/modules/models');
-
-app.listen(port);
-console.log(`listening to port`,port);
-
-const routes = require('./public/modules/routes');
-app.use('/', routes);
+app.get('/', function (req, res) {
+    res.render('pages/index');
+});
